e2ba7e6033945a6e3fb15024488130d481b0cce1
ProfessorF <professorf@gmail.com>
2020-12-13 18:16:37 -0700

    Added annotations for graphs

--- R/plotState.R
+++ R/plotState.R
@@ -1,6 +1,7 @@
-plotState = function (dfd, State, DataType) {
+plotState = function (dfd, State, DataType, dfa=NULL) {
   StateInfo=summarizeState(dfd, State)
   XBarPlot=barplot(StateInfo$StateVals,ylim=c(0, StateInfo$MaxDayVal), col="#CCCCCC", bor="white", xlab="Date", ylab=sprintf("# %s",DataType))
+  StateInfo[["XBarPlot"]]=XBarPlot # Add xvalues to list of return values
   Colors=RColorBrewer::brewer.pal(8, "Dark2")
   #
   # 7-day moving average
@@ -11,6 +12,21 @@ plotState = function (dfd, State, DataType) {
   #Avg4=sapply(4:(length(StateVals)-3), function(i) { mean(StateVals[(i-3):(i+3)])})
   #lines(XBarPlot[4:(length(StateVals)-3)], Avg4, col=Colors[1])
 
+  #
+  # Do any annotations
+  #
+  if (!is.null(dfa)) {
+    Dates=as.Date(names(dfd[2:length(names(dfd))]), format="X%m.%d.%y")
+    AnnotateDate  = dfa$AnnotateDate
+    AnnotateLabel = dfa$AnnotateLabel
+    YVal=StateInfo$MaxDayVal
+    XRow=sapply(AnnotateDate, function(x) { which(Dates==x) })
+    XVal=StateInfo$XBarPlot[XRow]
+    for (i in 1:length(XVal)) {
+      lines(c(XVal[i], XVal[i]), c(0,YVal), col="pink", lty=3)
+      text(XVal[i], YVal, AnnotateLabel[i], pos=1, col="gray") # 1 is below
+    }
+  }
   #
   # Title graph
   #
